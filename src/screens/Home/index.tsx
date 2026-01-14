fb68827600119276b906fe0c915ad53ff749b723
Himanshu Toppo <72000638+Himanshuxyzz@users.noreply.github.com>
2026-01-14 15:16:56 +0530

    Update index.tsx

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,56 +5,41 @@ import WebView, { WebViewMessageEvent } from "react-native-webview";
 import Theme from "@/styles/Theme";
 import { videoListHtml } from "./videoListHtml";
 import { useNavigation } from "@react-navigation/native";
-import LMSSkeleton from "@/components/common/LMSSkeleton";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootParamList } from "@/navigation";
+import HomeSkeleton from "@/components/Home/HomeSkeleton";
 
 const Home = () => {
-  const navigation = useNavigation();
+  const navigation = useNavigation<NativeStackNavigationProp<RootParamList>>();
   const [isLoading, setIsLoading] = useState(true);
 
   const handleMessage = (event: WebViewMessageEvent) => {
     try {
       const data = JSON.parse(event.nativeEvent.data);
       if (data.type === "openVideo") {
-        Alert.alert("Lesson Selected", `Opening lesson: ${data.lessonId}`);
-        // Handle video opening logic here
         navigation.navigate("VideoPlayer", { videoId: data.lessonId });
       }
     } catch (error) {
       console.log("Message received:", event.nativeEvent.data);
+      return;
     }
   };
 
   return (
     <Container edgeToEdge={true} style={{}}>
-      <View style={{ flex: 0.9 }}>
-        {isLoading && (
-          <View style={StyleSheet.absoluteFill}>
-            <LMSSkeleton />
-          </View>
-        )}
-        <WebView
-          source={{ html: videoListHtml }}
-          onMessage={handleMessage}
-          style={{ flex: 1, opacity: isLoading ? 0 : 1 }}
-          scrollEnabled={true}
-          onLoadEnd={() => setIsLoading(false)}
-        />
-      </View>
-      <View
-        style={{
-          flex: 0.1,
-          borderWidth: StyleSheet.hairlineWidth,
-          gap: Theme.SIZES.SM,
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "center",
-          padding: Theme.SIZES.MD,
-          backgroundColor: "#fff",
-        }}
-      >
-        <CustomButton text="Play" onPress={() => {}} />
-        <CustomButton text="Pause" onPress={() => {}} />
-      </View>
+      {isLoading && (
+        <View style={StyleSheet.absoluteFill}>
+          <HomeSkeleton />
+        </View>
+      )}
+      <WebView
+        showsVerticalScrollIndicator={false}
+        source={{ html: videoListHtml }}
+        onMessage={handleMessage}
+        style={{ flex: 1, opacity: isLoading ? 0 : 1 }}
+        scrollEnabled={true}
+        onLoadEnd={() => setIsLoading(false)}
+      />
     </Container>
   );
 };
