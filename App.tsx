332355f5fd6e7c992c7c1cf7ce3965f9a9a7fc96
Himanshu Toppo <72000638+Himanshuxyzz@users.noreply.github.com>
2026-02-26 20:05:08 +0530

    Feat: Added the sentry for error monitoring 👍

--- App.tsx
+++ App.tsx
@@ -1,9 +1,29 @@
 import React from "react";
 import RootNavigation from "@/navigation";
 import { StyleSheet } from "react-native";
+import * as Sentry from '@sentry/react-native';
 
-export default function App() {
+Sentry.init({
+  dsn: 'https://d2050e1ce07fa6fbac26db273622ed58@o4510952694874112.ingest.us.sentry.io/4510952697888768',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+export default Sentry.wrap(function App() {
   return <RootNavigation />;
-}
+});
 
 const styles = StyleSheet.create({});
